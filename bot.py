8dcd6e20a4a0f1becc8df083e55c09947ac59154
sahilkhurana02 <sahil_khurana@outlook.com>
2016-09-07 20:23:31 +0530

    Added Spotify API

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from telegram.ext import Updater, CommandHandler
 import telegram
 import logging
 import feedparser
+import requests
+import json
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -24,9 +26,29 @@ def news(bot, update):
 		description = feed.entries[i].description
 		bot.sendMessage(chat_id=update.message.chat_id, text="<b>" + title + "</b>" + "\n\n" + description, parse_mode = telegram.ParseMode.HTML)
 
+def songs(bot,update):
+	url = 'https://api.spotify.com/v1/users/spotify/playlists/5FJXhjdILmRA2z5bvz4nzf/tracks?limit=5'
+	headers = {'Authorization': 'Bearer BQAUTshSZjK_TgBhbZeF4KiSO6ZvLlR11piUXpOFtYyIoGgWNIZF3Uw1tbYB7s3WPdBT8nCiWY1UT3gBlsurah7OtS2ILhDekdOO9pxNil843GzA6fyLmfIoo524AR0aRcl0kdMqLeP2g_Q8VJNjKLRt0q-2N8XVZ0xg2om7'}
+	r = requests.get(url, headers=headers)
+	
+	#url = 'https://api.spotify.com/v1/users/spotify/playlists/5FJXhjdILmRA2z5bvz4nzf/tracks?limit=5'
+	#headers = {'Authorization': 'Bearer BQAUTshSZjK_TgBhbZeF4KiSO6ZvLlR11piUXpOFtYyIoGgWNIZF3Uw1tbYB7s3WPdBT8nCiWY1UT3gBlsurah7OtS2ILhDekdOO9pxNil843GzA6fyLmfIoo524AR0aRcl0kdMqLeP2g_Q8VJNjKLRt0q-2N8XVZ0xg2om7'}
+
+	#r = requests.get(url, headers=headers)
+	print(r)
+	data = r.json()
+	for i in range(5):
+		songName = data["items"][i]["track"]["name"]
+		artitstName = data["items"][i]["track"]["artists"][i]["name"]
+		previewUrl = data["items"][i]["track"]["preview_url"]
+		bot.sendMessage(chat_id=update.message.chat_id, text = songName + "\n" + artitstName + "\n" + previewUrl)
+	
+
 start_handler = CommandHandler('start', start)
 news_handler = CommandHandler('topnews', news)
+songs_handler = CommandHandler('topsongs', songs)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(news_handler)
+dispatcher.add_handler(songs_handler)
 
 updater.start_polling()
